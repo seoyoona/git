9ce6ca801fb4c5324a157413f434a3cea173a6bb
seoyoona <synsyn0311@gmail.com>
2023-08-24 16:59:21 +0900

    siteA_done

--- siteA/jquery.js
+++ siteA/jquery.js
@@ -1,15 +1,32 @@
-const depth1 =$('.depth1');
+const depth1 = $('.depth1');
 depth1.hover(
-    function() {$('.depth2').css('display','block'); $('header').addClass('on');},
-    function() {$('.depth2').css('display','none'); $('header').removeClass('on');}
+    function () { $('.depth2').css('display', 'block'); $('header').addClass('on'); },
+    function () { $('.depth2').css('display', 'none'); $('header').removeClass('on'); }
 )
 
-// depth1.addEventListener('mouseover', function () {
-//   depth1.querySelectorAll('.depth2').forEach((o) => { o.style.display = 'block' })
-//   document.querySelector('header').classList.add('on');
-// });
+// slide
+// = : 할당 연산자 (좌항에 우항의 값을 할당)
+const slides = $('.slide_wrap li');
+let n = 0;
+function slide() { slides.removeClass('on'); slides.eq(n).addClass('on'); n++; if (n >= slides.length) { n = 0 } } // 변수+1
+setInterval(() => { slide(); }, 2000);
 
-// depth1.addEventListener('mouseout', function () {
-//   depth1.querySelectorAll('.depth2').forEach((o) => { o.style.display = 'none' })
-//   document.querySelector('header').classList.remove('on');
-// });
+// tab
+const tabBtn = $('.board .buttons li');
+const panels = $('.panels>div');
+tabBtn.on('click', function (e) {
+    e.preventDefault(); let tg = $(this).index();
+    tabBtn.removeClass('on');
+    tabBtn.eq(tg).addClass('on');
+    panels.hide();
+    panels.eq(tg).show();
+});
+
+// popup
+$('.open').on('click', function (e) {
+    e.preventDefault();
+    $('.popup').show(); $('.overlay').addClass('open');});
+
+$('.close').on('click', function (e) {
+    e.preventDefault();
+    $('.popup').hide(); $('.overlay').removeClass('open');});
--- siteA/vanilla.js
+++ siteA/vanilla.js
@@ -8,3 +8,16 @@ depth1.addEventListener('mouseout', function () {
   depth1.querySelectorAll('.depth2').forEach((o) => { o.style.display = 'none' })
   document.querySelector('header').classList.remove('on');
 });
+
+// slide
+const slides=document.querySelectorAll('.slide_wrap li');
+let n=0;
+// .slide_wrap li의 on 클래스 삭제
+function slide(){slides.forEach((slide)=>{slide.classList.remove('on')});
+// 새로운 슬라이드에 'on' 클래스 추가
+slides[n].classList.add('on');
+  n++;
+  if(n >= slides.length){n=0}}
+setInterval(()=>{slide();}, 2000);
+
+slides[n].classList('on');
